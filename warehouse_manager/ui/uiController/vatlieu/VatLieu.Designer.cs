e098e5b6e1d72b3373d9c6869776a3bbfd6c418f
trong <cutocm989@gmail.com>
2025-09-11 16:58:31 +0700

    /

---
+++ warehouse_manager/ui/uiController/vatlieu/VatLieu.Designer.cs
@@ -0,0 +1,436 @@
+﻿namespace warehouse_manager.ui.uiController.vatlieu
+{
+    partial class VatLieu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            panel1 = new Panel();
+            label9 = new Label();
+            panel3 = new Panel();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            label10 = new Label();
+            label11 = new Label();
+            label5 = new Label();
+            panel2 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label12 = new Label();
+            comboBox5 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(1187, 70);
+            label1.TabIndex = 0;
+            label1.Text = "Quản lý vật liệu";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 465);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1181, 186);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 70F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1193, 730);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(dataGridView1, 0, 1);
+            tableLayoutPanel2.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 73);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 462F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 122F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.Size = new Size(1187, 654);
+            tableLayoutPanel2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.None;
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(78, 71);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1031, 319);
+            panel1.TabIndex = 4;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label9.Location = new Point(773, 14);
+            label9.Name = "label9";
+            label9.Size = new Size(44, 31);
+            label9.TabIndex = 19;
+            label9.Text = "lọc";
+            label9.Click += label9_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(comboBox3);
+            panel3.Controls.Add(comboBox4);
+            panel3.Controls.Add(label10);
+            panel3.Controls.Add(label11);
+            panel3.Location = new Point(585, 51);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(433, 256);
+            panel3.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(221, 41);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(180, 28);
+            comboBox3.TabIndex = 31;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(221, 7);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(180, 28);
+            comboBox4.TabIndex = 30;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label10.Location = new Point(63, 45);
+            label10.Name = "label10";
+            label10.Size = new Size(68, 23);
+            label10.TabIndex = 29;
+            label10.Text = "Tên loại";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label11.Location = new Point(61, 12);
+            label11.Name = "label11";
+            label11.Size = new Size(145, 23);
+            label11.TabIndex = 28;
+            label11.Text = "Tên nhà cung cấp";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label5.Location = new Point(192, 14);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 31);
+            label5.TabIndex = 17;
+            label5.Text = "chức năng";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(comboBox5);
+            panel2.Controls.Add(label12);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(comboBox2);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(14, 51);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(474, 256);
+            panel2.TabIndex = 16;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(368, 75);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 30;
+            button3.Text = "Xoá";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(368, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 29;
+            button2.Text = "Sửa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(368, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 28;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(166, 168);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(180, 28);
+            comboBox2.TabIndex = 27;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(166, 134);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(180, 28);
+            comboBox1.TabIndex = 26;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(166, 102);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(180, 27);
+            textBox4.TabIndex = 25;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(166, 69);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(180, 27);
+            textBox3.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(166, 36);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(180, 27);
+            textBox2.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(166, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(180, 27);
+            textBox1.TabIndex = 22;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label8.Location = new Point(8, 172);
+            label8.Name = "label8";
+            label8.Size = new Size(68, 23);
+            label8.TabIndex = 21;
+            label8.Text = "Tên loại";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label7.Location = new Point(6, 139);
+            label7.Name = "label7";
+            label7.Size = new Size(145, 23);
+            label7.TabIndex = 20;
+            label7.Text = "Tên nhà cung cấp";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label6.Location = new Point(6, 107);
+            label6.Name = "label6";
+            label6.Size = new Size(109, 23);
+            label6.TabIndex = 19;
+            label6.Text = "Số lượng tồn";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label4.Location = new Point(6, 40);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 23);
+            label4.TabIndex = 18;
+            label4.Text = "Đơn giá";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label3.Location = new Point(6, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 23);
+            label3.TabIndex = 17;
+            label3.Text = "Tên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label2.Location = new Point(6, 7);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 23);
+            label2.TabIndex = 16;
+            label2.Text = "Mã vật liệu";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            label12.Location = new Point(8, 204);
+            label12.Name = "label12";
+            label12.Size = new Size(94, 23);
+            label12.TabIndex = 31;
+            label12.Text = "Đơn vị tính";
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(166, 202);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(180, 28);
+            comboBox5.TabIndex = 32;
+            // 
+            // VatLieu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "VatLieu";
+            Size = new Size(1193, 730);
+            Load += VatLieu_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label9;
+        private Panel panel3;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Label label10;
+        private Label label11;
+        private Label label5;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private ComboBox comboBox5;
+        private Label label12;
+    }
+}
---
+++ warehouse_manager/ui/uiController/vatlieu/VatLieu.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using warehouse_manager.context;
+using warehouse_manager.Models;
+using warehouse_manager.service;
+
+namespace warehouse_manager.ui.uiController.vatlieu
+{
+    public partial class VatLieu : UserControl
+    {
+        private WarehouseManagerContext context = new WarehouseManagerContext();
+        public VatLieu()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool KiemTraNhapLieu()
+        {
+            // TextBox1: Mã vật liệu
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Mã liệu không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Focus();
+                return false;
+            }
+
+            // TextBox2: Đơn giá
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("TextBox2 không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox2.Focus();
+                return false;
+            }
+            if (!decimal.TryParse(textBox2.Text, out _))
+            {
+                MessageBox.Show("Đơn giá phải là số!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox2.Focus();
+                return false;
+            }
+
+            // TextBox3: Tên vật liệu
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Tên không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox3.Focus();
+                return false;
+            }
+
+            // TextBox4: Số lượng tồn
+            if (string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("số lượng tồn không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox4.Focus();
+                return false;
+            }
+            if (!int.TryParse(textBox4.Text, out _))
+            {
+                MessageBox.Show("Số lượng tồn phải là số nguyên!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox4.Focus();
+                return false;
+            }
+
+            // ComboBox1: Loại vật liệu
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Tên loại chưa được chọn!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBox1.Focus();
+                return false;
+            }
+
+            // ComboBox2: Nhà cung cấp
+            if (comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Tên nhà cung cấp chưa được chọn!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBox2.Focus();
+                return false;
+            }
+
+            // ComboBox5: Đơn vị tính
+            if (comboBox5.SelectedIndex == -1)
+            {
+                MessageBox.Show("Đơn vị tính chưa được chọn!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBox5.Focus();
+                return false;
+            }
+
+            return true; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var loai = context.LoaiVatLieus.FirstOrDefault(l => l.TenLoai == comboBox1.SelectedItem.ToString());
+                var ncc = context.NhaCungCaps.FirstOrDefault(n => n.TenNhaCungCap == comboBox2.SelectedItem.ToString());
+
+                if (loai == null || ncc == null)
+                {
+                    MessageBox.Show("Không tìm thấy loại vật liệu hoặc nhà cung cấp trong CSDL!");
+                    return;
+                }
+                if (!KiemTraNhapLieu()) return;
+
+                int sl = int.TryParse(textBox4.Text, out int result1) ? result1 : 0;
+
+                var vl = new Models.VatLieu
+                {
+                    MaVatLieu = textBox1.Text,
+                    DonGia = decimal.TryParse(textBox2.Text, out decimal result) ? result : 0,
+                    DonViTinh = comboBox5.SelectedItem.ToString(),
+                    Ten = textBox3.Text,
+                    SoLuongTon = sl,
+                    MaLoai = loai.Id,
+                    MaNhaCungCap = ncc.Id,
+                    TrangThai = sl > 0 ? "Còn hàng" : "Hết hàng"
+                };
+
+                context.VatLieus.Add(vl);
+                context.SaveChanges();
+
+                MessageBox.Show("Thêm vật liệu thành công!");
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi: " + ex.Message);
+            }
+        }
+
+        private void VatLieu_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            var ncc = new NhaCungCapService();
+            foreach(var item in ncc.danhSachNhaCungCap())
+            {
+                comboBox1.Items.Add(item);
+            }
+            var lvl = new LoaiVatLieuService();
+            foreach(var item in lvl.danhSachLoaiVatLieu())
+            {
+                comboBox2.Items.Add(item);
+            }
+            List<String> donViTinhs = new List<string>
+            {
+                "",
+                "Cái",
+                "Chiếc",
+                "Bộ",
+                "Hộp",
+                "Thùng",
+                "Kg",
+                "Gram",
+                "Mét",
+                "Mét Vuông",
+                "Mét Khối",
+                "Lít",
+                "Chiều"
+            };
+            foreach (var item in donViTinhs) {
+                comboBox5.Items.Add(item);
+            }
+        }
+        private void LoadData()
+        {
+            dataGridView1.DataSource = context.VatLieus
+                .Include(vl => vl.MaNhaCungCapNavigation)
+                .Include(vl => vl.MaLoaiNavigation)
+                .Select(
+                vl => new
+                {
+                    MaVatLieu = vl.MaVatLieu,
+                    DonGia = vl.DonGia,
+                    DonViTinh = vl.DonViTinh,
+                    Ten = vl.Ten,
+                    SoLuongTon = vl.SoLuongTon,
+                    LoaiVatLieu = vl.MaLoaiNavigation.TenLoai,
+                    TenNhaCungCap = vl.MaNhaCungCapNavigation.TenNhaCungCap,
+                    TrangThai = vl.TrangThai
+                }
+            ).ToList(); ;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
